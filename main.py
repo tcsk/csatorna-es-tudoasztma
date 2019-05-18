116c194d7d11ec03f7d1cc34e7c5b0c0d604bf8c
Tibor Nagy <tibornagy77@gmail.com>
2019-05-18 09:26:43 +0200

    matplotlib és figure csomag hozzáadása és importálása

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pan
+import matplotlib.pyplot as plot
+from matplotlib.pyplot import figure
+
 csatorna = pan.read_excel('./data/2_3_8i.xls', skiprows = [0, 1, 2, 3], sheet_name = '2.3.8.')
 csatorna = csatorna.drop(csatorna.columns[[1, 2, 3, 4, 5, 7, 8]], axis = 1)
 csatorna = csatorna.rename(index = str, columns = {csatorna.columns[0]: 'ev', csatorna.columns[1]: 'kozseg%'})

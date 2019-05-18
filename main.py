a8edaa37f0f25012504621757ae01144dd198131
Tibor Nagy <tibornagy77@gmail.com>
2019-05-18 09:24:39 +0200

    Felesleges oszlopok elhagyása

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pandas as pan
 csatorna = pan.read_excel('./data/2_3_8i.xls', skiprows = [0, 1, 2, 3], sheet_name = '2.3.8.')
+csatorna = csatorna.drop(csatorna.columns[[1, 2, 3, 4, 5, 7, 8]], axis = 1)
 print(csatorna)

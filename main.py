4b25e01c8bbf032997196715e65b2ee5173dc051
Tibor Nagy <tibornagy77@gmail.com>
2019-05-18 09:28:48 +0200

    Csatorna adatsor kirajzolása

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ csatorna = pan.read_excel('./data/2_3_8i.xls', skiprows = [0, 1, 2, 3], sheet_na
 csatorna = csatorna.drop(csatorna.columns[[1, 2, 3, 4, 5, 7, 8]], axis = 1)
 csatorna = csatorna.rename(index = str, columns = {csatorna.columns[0]: 'ev', csatorna.columns[1]: 'kozseg%'})
 print(csatorna)
+
+figure(num = None, figsize = (8, 8), dpi = 80, facecolor = 'w', edgecolor = 'k')
+plot.clf()
+plot.xlabel('Ev')
+plot.ylabel('Kozuzemi szennyvizgyujto-halozattal ellatott kozsegi lakasok aranya [%]')
+plot.plot(csatorna['ev'], csatorna['kozseg%'])
+plot.show()
